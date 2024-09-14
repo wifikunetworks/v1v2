7fef921b870815cfd25f9a5618211c3940f84fb4
wifikunetworks <163966198+wifikunetworks@users.noreply.github.com>
2024-09-15 00:14:16 +0700

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -7,7 +7,7 @@ CRON_FILE="/etc/crontabs/root"
 CRON_CONTENT="0 * * * * /sbin/free.sh >/dev/null 2>&1
 0 * * * * /usr/bin/autotimesync.sh
 */5 * * * * /www/vnstati/vnstati.sh >/dev/null 2>&1
-0 */2 * * * echo \"AT+CMGD=1,4\" | atinout - \"/dev/ttyACM2\" -
+0 2 * * * echo "AT+CMGD=1,4" | atinout - "/dev/ttyACM2" -
 0 3 * * * /sbin/reboot"
 
 # Mengganti isi file crontab
