9cfb1607d5bd68869a5a277150c78df3f7468513
wifikunetworks <163966198+wifikunetworks@users.noreply.github.com>
2025-02-24 20:31:54 +0700

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -8,7 +8,7 @@ CRON_CONTENT="0 * * * * /sbin/free.sh >/dev/null 2>&1
 0 * * * * /usr/bin/autotimesync.sh
 */5 * * * * /www/vnstati/vnstati.sh >/dev/null 2>&1
 0 2 * * * echo "AT+CMGD=1,4" | atinout - "/dev/ttyACM2" -
-0 */6 * * * /etc/init.d/tailscale restart
+0 */6 * * * /etc/init.d/tailscale restart"
 
 # Mengganti isi file crontab
 echo "$CRON_CONTENT" > "$CRON_FILE"
